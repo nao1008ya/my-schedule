f18a89e1929e5930aaeb2fabfb3b7f364f054c6d
nao1008ya <sinnao1008@gmail.com>
2020-11-05 15:14:05 +0900

    編集、削除機能実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
   def index
     @events = Event.all
   end
@@ -7,14 +8,39 @@ class EventsController < ApplicationController
     @event = Event.new
   end
   def create
-    Event.create(event_parameter)
+    Event.create(event_params)
     redirect_to root_path
   end
 
+  def show
+  end
+
+  def edit
+  end
+  def update
+    if @event.update(event_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @event.user.id == current_user.id
+      @event.destroy
+      redirect_to root_path
+    else
+      redirect_to action: :index
+    end
+  end
 
   private
 
-  def event_parameter
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
     params.require(:event).permit(:title, :content, :start_time).merge(user_id: current_user.id)
   end
 end
