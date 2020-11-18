152b89bc518563c76449f0f8ac95ebdb4c782ecb
nao1008ya <sinnao1008@gmail.com>
2020-11-18 15:45:22 +0900

    chat送信機能の実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,4 +3,20 @@ class ChatsController < ApplicationController
     @chat = Chat.new
     @group = Group.find(params[:group_id])
   end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @chat = @group.chats.new(chat_params)
+    if @chat.save
+      redirect_to group_chats_path(@group)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:text).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
 end
